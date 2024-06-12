94e2b778222c1f500aef07ddb43179b4927359a6
mayaraskin <mayaraskin14@gmail.com>
2024-06-12 17:52:47 +0300

    Added skeleton for revert_update func on reverts.service.ts

--- src/modules/backends-orchestrator/backends/reverts/reverts.service.ts
+++ src/modules/backends-orchestrator/backends/reverts/reverts.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotImplementedException } from '@nestjs/common';
 import { TRedisDocument } from '../../../../config/redis/redis.service';
 import { BackendsOrchestratorService } from '../../backends_orchestrator.service';
 import { BackendUrl } from '../../../../common';
@@ -15,6 +15,15 @@ export class RevertsService {
   constructor(
     private readonly backends_orchestrator: BackendsOrchestratorService,
   ) {}
+
+  private reverts: TRevert = {
+    credentials: {
+      POST: this.hard_delete_credentials,
+      DELETE: this.restore_credentials,
+      PUT: this.revert_update,
+    },
+  };
+
   private async hard_delete_credentials(info: TRedisDocument): Promise<void> {
     const uri = '/credentials';
 
@@ -46,12 +55,10 @@ export class RevertsService {
     await this.backends_orchestrator.make_request(config, info.backend);
   }
 
-  private reverts: TRevert = {
-    credentials: {
-      POST: this.hard_delete_credentials,
-      DELETE: this.restore_credentials,
-    },
-  };
+  private async revert_update(): Promise<void> {
+    throw new NotImplementedException();
+  }
+
   async revert(info: TRedisDocument) {
     if (!(info.uri in this.reverts) || !(info.method in this.reverts[info.uri]))
       return;
