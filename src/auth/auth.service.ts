9c12ea1d428b25159ae5132362368c920c7e11c4
mayaraskin <mayaraskin14@gmail.com>
2024-07-25 21:50:35 +0300

    Implemented login, validate_master_password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,37 +1,24 @@
 import { Injectable, Logger, NotImplementedException } from '@nestjs/common';
-import { BasicAuthLoginDTO } from './dto/basic_auth_login.dto';
-import { UserService } from '../modules/user/user.service';
 import { Request } from 'express';
-import { CreateUserDTO } from '../modules/user/dto/create_user.dto';
-import { OTPService } from '../otp/otp.service';
 import { ResponseDTO } from '../common';
 import { AccessTokenResponseDTO } from './dto/access_token_response.dto';
+import { BackendsOrchestratorService } from '../modules/backends-orchestrator/backends_orchestrator.service';
 
 @Injectable()
 export class AuthService {
   private readonly logger = new Logger(AuthService.name);
   constructor(
-    private readonly user_service: UserService,
-    private readonly otp_service: OTPService,
+    private readonly backends_orchestrator: BackendsOrchestratorService,
   ) {}
-  async login(
-    req: Request,
-    user: BasicAuthLoginDTO,
-  ): Promise<AccessTokenResponseDTO> {
+  login(req: Request): Promise<AccessTokenResponseDTO> {
+    return this.backends_orchestrator.redirect_request(req);
+  }
+
+  async register(req: Request): Promise<ResponseDTO> {
     throw new NotImplementedException();
   }
 
-  async register(
-    req: Request,
-    register_dto: CreateUserDTO,
-  ): Promise<ResponseDTO> {
-    throw new NotImplementedException();
-  }
-
-  async validate_master_password(
-    req: Request,
-    master_password: string,
-  ): Promise<ResponseDTO> {
-    throw new NotImplementedException();
+  async validate_master_password(req: Request): Promise<ResponseDTO> {
+    return this.backends_orchestrator.redirect_request(req);
   }
 }
