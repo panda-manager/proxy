23ad49335c0fb7d8ae07c1001e8fb5ef69579446
mayaraskin <mayaraskin14@gmail.com>
2024-08-16 15:25:17 +0300

    Added check for key exists when deleting a key

--- src/modules/backends-orchestrator/backends/reverts/reverts.service.ts
+++ src/modules/backends-orchestrator/backends/reverts/reverts.service.ts
@@ -1,9 +1,4 @@
-import {
-  forwardRef,
-  Inject,
-  Injectable,
-  NotImplementedException,
-} from '@nestjs/common';
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { BackendsOrchestratorService } from '../../backends_orchestrator.service';
 import { AxiosRequestConfig, RawAxiosRequestHeaders } from 'axios';
 import { TRevertsMap } from '../../../../common';
@@ -31,7 +26,8 @@ export class RevertsService {
       method: 'DELETE',
       url: uri,
       data: {
-        ...info.body,
+        host: info.body['host'],
+        login: info.body['login'],
         type: 'hard',
       },
       headers: info.headers as RawAxiosRequestHeaders,
@@ -46,15 +42,50 @@ export class RevertsService {
     const config: AxiosRequestConfig = {
       method: 'PUT',
       url: uri,
-      data: info.body,
+      data: {
+        host: info.body['host'],
+        login: info.body['login'],
+      },
       headers: info.headers as RawAxiosRequestHeaders,
     };
 
     await this.backendsOrchestratorService.makeRequest(config, info.backend);
   }
 
-  private async revertUpdateCredentials(): Promise<void> {
-    throw new NotImplementedException();
+  private async revertUpdateCredentials(info: RevertSchema): Promise<void> {
+    const deleteUri = '/credentials';
+
+    const deleteConfig: AxiosRequestConfig = {
+      method: 'DELETE',
+      url: deleteUri,
+      data: {
+        host: info.body['host'],
+        login: info.body['login'],
+        type: 'hard',
+      },
+      headers: info.headers as RawAxiosRequestHeaders,
+    };
+
+    await this.backendsOrchestratorService.makeRequest(
+      deleteConfig,
+      info.backend,
+    );
+
+    const restoreUri = '/credentials/restore';
+    const restoreConfig: AxiosRequestConfig = {
+      method: 'PUT',
+      url: restoreUri,
+      data: {
+        host: info.body['host'],
+        login: info.body['login'],
+      },
+      headers: info.headers as RawAxiosRequestHeaders,
+    };
+
+    await this.backendsOrchestratorService.makeRequest(
+      restoreConfig,
+      info.backend,
+    );
   }
 
   async revert(info: RevertSchema): Promise<void> {
